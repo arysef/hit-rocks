65d84a8639dbb98224a29eeb0ec20cd3cb49ebbc
Aryan Sefidi <asefidi@microsoft.com>
2021-10-16 14:20:26 -0500

    Initial Commit

---
+++ hitRocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hitRocks
+//
+//  Created by Aryan Sefidi on 10/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
